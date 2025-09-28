b4232c3cfb472bbcba1c177820d2dc9b718190e6
ds-bhatia <dhruvbhatia.2212@gmail.com>
2025-09-28 23:30:52 +0530

    Added upgrades class

---
+++ upgrade.gd
@@ -0,0 +1,4 @@
+class_name Upgrade extends Node
+
+enum Upgrades {cigarette, beer, magGlass, handcuff, unoRev, wildCard,
+ 			   disableUpgrade, expiredMed, inverter, burnerPhone, adrenaline, handSaw}
