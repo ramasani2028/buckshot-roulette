577cabe677f6a33843d0982b2e2f0980156f7c02
Don Roy Chacko <donisepic30@gmail.com>
2025-09-30 18:34:32 +0530

    feat: added more necessary game logic functions, and random generation of bullets

--- gameManager.gd
+++ gameManager.gd
@@ -12,7 +12,7 @@ class GameState:
 var gameState: GameState = GameState.new([], [])
 var players: Array[Player] = []
 var currPlayerTurnIndex: int = 0 
-var shotgunShells: Array[int] = []
+var shotgunShells: Array[int] = [] # 0 for blank, 1 for live
 var tableUpgrades: Array[Upgrades] = []
 var roundIndex: int = 0;
 var shotgunShellCount: int = 8; # some logic based on round index
@@ -23,7 +23,7 @@ func initMatch() -> void:
 	shotgunShellCount = 8;
 
 func initRound() -> void:
-	currPlayerTurnIndex = 1
+	currPlayerTurnIndex = 0
 
 func endTurn() -> void:
 	currPlayerTurnIndex += 1
@@ -31,25 +31,41 @@ func endTurn() -> void:
 func checkWin() -> bool:
 	return alivePlayers.size() == 1;
 
+func generateRandomBullets():
+	shotgunShells.clear()
+	for i in range(shotgunShellCount):
+		var shell = randi() % 2  
+		shotgunShells[i] = shell
 
 # Below are all functions that are player facing, call these when designing players for player devs
-func getGameState() -> void:
-	
-func getAlivePlayers() -> Array[Player]:
-	
-func getAllPlayers() -> Array[Player]:
-	
-
-# TODO: make upgrade class please
-func getUpgradesOnTheTable() -> Array[Upgrade]:
-	
+func endGame() -> void:
+	return
+func getGameState() -> GameState:
+	return gameState
 
 func shootPlayer(callerPlayerRef: Player, targetPlayerRef: Player) -> void:
 	# logic for shooting
+	if checkWin():
+		endGame()
 	endTurn()
 
-func pickUpUpgrade(callerPlayerRef: Player, upgradeRef: Upgrade) -> void:
-	
+# call this when you want to pick an upgrade off of the upgrade table
+# returns true if succesffuly picked up
+# otherwise returns false
+func pickUpUpgrade(callerPlayerRef: Player, upgradeRef: Upgrade) -> boolean:
+	# im guessing i need to include logic in the scene to actually add and remove upgrades from the table visually
+	var gotUpgrade: bool = false
+	var upIndex: int = -1
+	for i in tableUpgrades.size():
+		if upgradeRef == tableUpgrades[i]:
+			gotUpgrade = true
+			upIndex = i
+	if gotUpgrade:
+		callerPlayerRef.addInventory(upgradeRef)
+		tableUpgrades.pop_at(i)
+	else:
+		return false
+	return true
 
 func useUpgrade(upgradeRef: Upgrade, callerPlayerRef: Player, targetPlayerRef: Player) -> void:
 	
