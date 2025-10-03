1517843e2e78e2b76bd5bd9f86ed9ec87569ad0a
Don Roy Chacko <donisepic30@gmail.com>
2025-10-03 12:25:07 +0530

    feat: completed logic of the game loop, work thats left completely depends on scene

--- scripts/gameManager.gd
+++ scripts/gameManager.gd
@@ -7,30 +7,92 @@ var currPlayerTurnIndex: int = 0
 var shotgunShells: Array[int] = [] # 0 for blank, 1 for live
 var roundIndex: int = 0
 var shotgunShellCount: int = 8 # some logic based on round index
+var initShotgunShellCount: int = 8
+var minRealShots: int = 2
+var realShots: int = 0;
+var blanks: int = 0;
 var maxHP: int = 3 # temporary value
-
+var isUpgradeRnd : bool = false # false means players can shoot, true means its upgrade pickup round
 # Game Logic functions
 func initMatch() -> void:
+	# add logic here to set up initial players and scene (can only really do this once the scene is done)
+	# scene work ig would need to be done to actually call this func
+	# one this fumc is called though a continuous match SHOULD work.
+	# as for UI changes and stuff best to have them as side effects of functions here i think.
 	roundIndex = 0
 	shotgunShellCount = 8
+	initRound()
 
 func initRound() -> void:
-	currPlayerTurnIndex = 0
+	# figure out a way to randomly generate upgrade scenes and spawn them into the world
+	if roundIndex != 1:
+		generateRandomUpgrades() # doesnt work yet
+	
+	shotgunShellCount = initShotgunShellCount * (roundIndex + 1) # maybe give this more thought
+	# use real and blanks to show at the start of a round for a bit
+	realShots = randi() % (shotgunShellCount - minRealShots) + minRealShots
+	blanks = shotgunShellCount - realShots
+	generateRandomBulletsOrder() # aka shuffle
+	if roundIndex != 1:
+		isUpgradeRnd = true
+	currPlayerTurnIndex = randi() % gameState.alivePlayers.size()
+	
 
 func endTurn() -> void:
-	currPlayerTurnIndex += 1
+	if(shotgunShells.size() == 0):
+		roundIndex += 1
+		initRound()
+		return
+	
+	currPlayerTurnIndex = (currPlayerTurnIndex + 1) % gameState.alivePlayers.size()
+	
+	if isUpgradeRnd && gameState.upgradesOnTable.size() == 0:
+		isUpgradeRnd = false
+		currPlayerTurnIndex -= 1
+		if(currPlayerTurnIndex == -1):
+			currPlayerTurnIndex = gameState.alivePlayers.size() - 1
+		# to ensure the player who picked the last upgrade gets the first shot
+		# if we are adding UI based on this change the above
+		
+		# now here would probably be a good time to flash the number of reals and blanks
+		 
 
+	# maybe we could allow players to use upgrades like handcuffs during the upgrade pickup sesh?
+	while gameState.alivePlayers[currPlayerTurnIndex].isHandcuffed:
+		gameState.alivePlayers[currPlayerTurnIndex].isHandcuffed = false # in the actual buckshot roulette handcuffs are broken after a full circle of the skipped turn so might have to change this
+		currPlayerTurnIndex = (currPlayerTurnIndex + 1) % gameState.alivePlayers.size()
+	
 func checkWin() -> bool:
 	return gameState.alivePlayers.size() == 1
 
-func generateRandomBullets():
+func generateRandomBulletsOrder():
 	shotgunShells.clear()
+	var remainingReal = realShots
+	var remainingBlank = blanks
+
 	for i in range(shotgunShellCount):
-		var shell = randi() % 2  
-		shotgunShells[i] = shell
+		if remainingReal > 0 && remainingBlank > 0:
+			var choice = randi() % 2
+			if choice == 1:
+				shotgunShells.append(1)
+				remainingReal -= 1
+			else:
+				shotgunShells.append(0)
+				remainingBlank -= 1
+		elif remainingReal > 0:
+			shotgunShells.append(1)
+			remainingReal -= 1
+		elif remainingBlank > 0:
+			shotgunShells.append(0)
+			remainingBlank -= 1
+
+func generateRandomUpgrades():
+	# wait for enum ig
+	pass
 
 # Below are all functions that are player facing, call these when designing players for player devs
 func endGame() -> void:
+	# do something like announce winner or change ui here later
 	return
 
 func getGameState() -> GameState:
@@ -41,27 +103,38 @@ func shootPlayer(callerPlayerRef: Player, targetPlayerRef: Player) -> void:
 	# reset callerPlayerRef power to 1 after each shot (handSaw might have been used)	
 	# also remove handcuff from player after their turn has been skipped (idk where to put this comment)
 	# maybe in endTurn(), keep skipping until it finds a player not handcuffed?
+	if(isUpgradeRnd):
+		return
+	if(callerPlayerRef != gameState.alivePlayers[currPlayerTurnIndex]):
+		return # not ur goddamn turn
+	var currBull : int = shotgunShells.pop_front()
+	var dmg = currBull * callerPlayerRef.power
+	targetPlayerRef.takeDamage(dmg)
+	if(targetPlayerRef.hp == 0):
+		for i in range(gameState.alivePlayers.size()):
+			if(gameState.alivePlayers[i] == targetPlayerRef):
+				gameState.alivePlayers.pop_at(i)
+				break
+	callerPlayerRef.power = 1
 	if checkWin():
 		endGame()
 	endTurn()
 
 # call this when you want to pick an upgrade off of the upgrade table
-# returns true if succesffuly picked up
-# otherwise returns false
-func pickUpUpgrade(callerPlayerRef: Player, upgradeRef: Upgrade) -> bool:
-	# im guessing i need to include logic in the scene to actually add and remove upgrades from the table visually
-	var gotUpgrade: bool = false
-	var upIndex: int = -1
-	for i in gameState.upgradesOnTable.size():
-		if upgradeRef == gameState.upgradesOnTable[i]:
-			gotUpgrade = true
-			upIndex = i
-	if gotUpgrade:
-		callerPlayerRef.addInventory(upgradeRef)
-		gameState.upgradesOnTable.pop_at(upIndex)
-	else:
-		return false
-	return true
+func pickUpUpgrade(callerPlayerRef: Player, upgradeRef: Upgrade) -> void:
+	if !isUpgradeRnd:
+		return
+	if(callerPlayerRef != gameState.alivePlayers[currPlayerTurnIndex]):
+		return # not ur goddamn turn
+		
+	for i in range(gameState.upgradesOnTable.size()):
+		if gameState.upgradesOnTable[i] == upgradeRef:
+			callerPlayerRef.addInventory(upgradeRef)
+			gameState.upgradesOnTable.pop_at(i)
+			break
+	endTurn()
+	
+	
 	
 # TODO: need to add logic for specific upgrades, e.g. cannot use handsaw when already used (power already 2).
 func useUpgrade(upgradeRef: Upgrade, callerPlayerRef: Player, targetPlayerRef: Player = null) -> void:
