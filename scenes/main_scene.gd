13783cfa8c841150f2c8667cd73d4c22a740e135
ds-bhatia <dhruvbhatia.2212@gmail.com>
2025-10-04 16:59:40 +0530

    Added comment for viewport

--- scenes/main_scene.gd
+++ scenes/main_scene.gd
@@ -3,7 +3,7 @@ extends Node
 @export var gameScene: PackedScene
 
 func _ready():
-	get_viewport().set_embedding_subwindows(false)
+	get_viewport().set_embedding_subwindows(false) #important
 	# Player 1
 	var game1 = gameScene.instantiate()
 	add_child(game1)
@@ -19,5 +19,5 @@ func _ready():
 	var game2 = gameScene.instantiate()
 	window2.add_child(game2)
 	game2.get_node("Player2/Camera3D").current = true
-	window2.get_viewport().world_3d = game1.get_viewport().world_3d
+	window2.get_viewport().world_3d = game1.get_viewport().world_3d #also important (gives them common world)
 	
