282fe99dc46ac42bb583e61901b434eefd747d54
Don Roy Chacko <donisepic30@gmail.com>
2025-10-05 05:09:20 +0530

    feat: implement upgrade generation from jeff, and randomly spawn instances when its an upgrade round, placement needs alot of work though just did a simple square grid for now

--- scripts/gameManager.gd
+++ scripts/gameManager.gd
@@ -2,7 +2,8 @@ extends Node
 
 # Initializing the game state
 var gameState: GameState = null
-@onready var players: Array[Player] = []
+var upgradeScene: PackedScene = null
+var players: Array[Player] = []
 var currPlayerTurnIndex: int = 0 
 var shotgunShells: Array[int] = [] # 0 for blank, 1 for live
 var roundIndex: int = 0
@@ -13,25 +14,27 @@ var realShots: int = 0;
 var blanks: int = 0;
 var maxHP: int = 3 # temporary value
 var isUpgradeRnd : bool = false # false means players can shoot, true means its upgrade pickup round
+var table: Node3D = null;
 # Game Logic functions
 func initMatch() -> void:
 	# add logic here to set up initial players and scene (can only really do this once the scene is done)
 	# scene work ig would need to be done to actually call this func
 	# one this fumc is called though a continuous match SHOULD work.
 	# as for UI changes and stuff best to have them as side effects of functions here i think.
+	upgradeScene = preload("res://scenes/upgrade.tscn")
 	roundIndex = 0
 	shotgunShellCount = 8
-	print(get_node("../Player1"))
 	players = [get_node("../Player1"), get_node("../Player2")]
-	print(players)
-	return;
 	gameState = GameState.new(players, [])
+	table = get_node("../Table")
 	initRound()
 
 func initRound() -> void:
+	
 	# figure out a way to randomly generate upgrade scenes and spawn them into the world
 	if roundIndex != 0:
 		generateRandomUpgrades() # doesnt work yet
+		spawnUpgradesOnTable()
 	
 	shotgunShellCount = initShotgunShellCount * (roundIndex + 1) # maybe give this more thought
 	# use real and blanks to show at the start of a round for a bit
@@ -41,7 +44,6 @@ func initRound() -> void:
 	if roundIndex != 0:
 		isUpgradeRnd = true
 	currPlayerTurnIndex = randi() % gameState.alivePlayers.size()
-	
 
 func endTurn() -> void:
 	if(shotgunShells.size() == 0):
@@ -51,6 +53,9 @@ func endTurn() -> void:
 	
 	currPlayerTurnIndex = (currPlayerTurnIndex + 1) % gameState.alivePlayers.size()
 	
+	if(isUpgradeRnd):
+		spawnUpgradesOnTable()
+		
 	if isUpgradeRnd && gameState.upgradesOnTable.size() == 0:
 		isUpgradeRnd = false
 		currPlayerTurnIndex -= 1
@@ -61,7 +66,9 @@ func endTurn() -> void:
 		
 		# now here would probably be a good time to flash the number of reals and blanks
 		 
-
+	# once all turn loggic is done with, send a signal to all players to refetch gameState
+	
+	
 	# maybe we could allow players to use upgrades like handcuffs during the upgrade pickup sesh?
 	while gameState.alivePlayers[currPlayerTurnIndex].isHandcuffed:
 		gameState.alivePlayers[currPlayerTurnIndex].isHandcuffed = false # in the actual buckshot roulette handcuffs are broken after a full circle of the skipped turn so might have to change this
@@ -92,8 +99,58 @@ func generateRandomBulletsOrder():
 			remainingBlank -= 1
 
 func generateRandomUpgrades():
-	# wait for enum ig
-	pass
+	# cleaned up jeff code 
+	gameState.upgradesOnTable.clear()
+	var numUpgrades = 8 * roundIndex
+	var availableTypes = []
+	if roundIndex == 1:
+		availableTypes = [
+			Upgrade.UpgradeType.cigarette,
+			Upgrade.UpgradeType.beer,
+			Upgrade.UpgradeType.magGlass
+		]
+	elif roundIndex == 2:
+		availableTypes = [
+			Upgrade.UpgradeType.cigarette,
+			Upgrade.UpgradeType.beer,
+			Upgrade.UpgradeType.magGlass,
+			Upgrade.UpgradeType.handSaw,
+			Upgrade.UpgradeType.handcuff
+		]
+	else:
+		availableTypes = Upgrade.UpgradeType.values()
+
+	for i in range(numUpgrades):
+		var randomType = availableTypes[randi() % availableTypes.size()]
+		var newUpgrade = Upgrade.new()
+		newUpgrade.upgrade_type = randomType
+		gameState.upgradesOnTable.append(newUpgrade)
+
+# curr plan is to recall this every upgrade turn
+func spawnUpgradesOnTable():
+	for child in table.get_children():
+		if child is Upgrade:
+			child.queue_free()
+	
+	# hopefully a square grid forms of side 1.5 1.5, temp for now 
+	var tableX : float = 1.5
+	var tableZ: float = 1.5
+	var columns : int = ceil(sqrt(gameState.upgradesOnTable.size()))
+	var rows : int = ceil(sqrt(gameState.upgradesOnTable.size()))
+	var spacingX = tableX/columns
+	var spacingZ = tableZ/rows
+	var startX = -0.5   # messy magic number offset temporary for now
+	var startZ = 4.5 # messy magic number offset temporary for now
+	
+	for i in range(gameState.upgradesOnTable.size()):
+		var upgradeInstance: Upgrade = upgradeScene.instantiate() as Upgrade
+		upgradeInstance.upgrade_type = gameState.upgradesOnTable[i].upgrade_type
+		var row = i/columns # how to disable stupid ass warning for int div
+		var col = i % columns
+		upgradeInstance.position = Vector3(startX + col * spacingX,  0.1, (startZ + row * spacingZ))
+		gameState.upgradesOnTable[i].pos = upgradeInstance.position
+		print(gameState.upgradesOnTable[i].pos)
+		table.add_child(upgradeInstance)
 
 # Below are all functions that are player facing, call these when designing players for player devs
 func endGame() -> void:
@@ -168,7 +225,8 @@ func useUpgrade(upgradeRef: Upgrade, callerPlayerRef: Player, targetPlayerRef: P
 			useDisableUpgrade(callerPlayerRef, targetPlayerRef)
 		Upgrade.UpgradeType.wildCard:
 			# Generating random upgrade from 0-7
-			var newUpgrade = Upgrade.new(Upgrade.UpgradeType.values()[randi() % 8])
+			var newUpgrade = Upgrade.new()
+			newUpgrade.upgrade_type = Upgrade.UpgradeType.values()[randi() % 8]
 			useUpgrade(newUpgrade, callerPlayerRef, targetPlayerRef)
 		
 	callerPlayerRef.inventory.erase(upgradeRef)
