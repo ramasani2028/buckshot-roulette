89cf022989071b6b706a76fe633ee761586e787e
ds-bhatia <dhruvsbhatia@gmail.com>
2025-10-05 15:01:06 +0530

    Fixed multiple windows

--- scripts/mainScene.gd
+++ scripts/mainScene.gd
@@ -1,26 +1,36 @@
 extends Node
-
 @export var gameScene: PackedScene
 
 func _ready():
 	get_viewport().set_embedding_subwindows(false) #important
+
+	var game = gameScene.instantiate()
+	add_child(game)
 	
 	# Player 1
+	game.get_node("Player1/Camera3D").current = true
 	get_window().title = "Player 1"
-	var game1 = gameScene.instantiate()
-	add_child(game1)
-	game1.get_node("Player1/Camera3D").current = true
-	
-	#Player 2
+
+	# Player 2
 	var window2 = Window.new()
-	window2.size = Vector2(1152,648)
-	window2.visible = true
+	window2.size = Vector2(1152, 648)
 	window2.position = Vector2(860, 340)
+	window2.visible = true
 	window2.title = "Player 2"
 	add_child(window2)
-	
-	var game2 = gameScene.instantiate()
-	window2.add_child(game2)
-	game2.get_node("Player2/Camera3D").current = true
-	window2.get_viewport().world_3d = game1.get_viewport().world_3d #also important (gives them common world)
-	
+
+	var container = SubViewportContainer.new()
+	window2.add_child(container)
+
+	var sv = SubViewport.new()
+	sv.size = Vector2(1152, 648)
+	sv.world_3d = get_viewport().world_3d
+	sv.render_target_update_mode = SubViewport.UPDATE_ALWAYS
+	container.add_child(sv)
+
+	var cam2 = game.get_node("Player2/Camera3D")
+	var cam_global = cam2.global_transform
+	cam2.get_parent().remove_child(cam2) #removing camera from player node, kinda inefficient but works
+	sv.add_child(cam2)
+	cam2.global_transform = cam_global
+	cam2.current = true
